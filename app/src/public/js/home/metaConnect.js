e0a1be824852d571f39c5f88d8ef2d1e6d638b77
RBG <roundbeargames@gmail.com>
2022-04-24 22:53:59 +0900

    delete old code

--- app/src/public/js/home/metaConnect.js
+++ app/src/public/js/home/metaConnect.js
@@ -1,44 +1,15 @@
 "use strict"
 
-console.log("running metaConnect.js");
-
 window.userWalletAddress = null;
-
-const loginButton = document.getElementById('loginButton');
-const userWallet = document.getElementById('userWallet');
 
 const connectButton = document.getElementById("connectButton");
 
-console.log(connectButton);
-
 function toggleButton() {
-    if (!window.ethereum) {
-        loginButton.innerText = 'MetaMask is not installed'
-        loginButton.classList.remove('bg-purple-500', 'text-white')
-        loginButton.classList.add('bg-gray-500', 'text-gray-100', 'cursor-not-allowed')
-        return false
+    if (window.ethereum) {
+        connectButton.addEventListener("click", connectWithMetaMask);
+    } else {
+        //send player to onboarding page
     }
-
-    //loginButton.addEventListener('click', loginWithMetaMask);
-    connectButton.addEventListener("click", connectWithMetaMask);
-}
-
-async function loginWithMetaMask() {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-        .catch((e) => {
-            console.error(e.message)
-            return
-        })
-    if (!accounts) { return }
-
-    window.userWalletAddress = accounts[0]
-    userWallet.innerText = window.userWalletAddress
-    loginButton.innerText = 'Sign out of MetaMask'
-
-    loginButton.removeEventListener('click', loginWithMetaMask)
-    setTimeout(() => {
-        loginButton.addEventListener('click', signOutOfMetaMask)
-    }, 200)
 }
 
 async function connectWithMetaMask() {
@@ -54,7 +25,7 @@ async function connectWithMetaMask() {
         const walletPub = accounts[0];
         console.log("wallet connected: " + walletPub);
 
-        connectButton.removeEventListener('click', loginWithMetaMask)
+        connectButton.removeEventListener('click', connectWithMetaMask)
 
         const req = {
             walletAddress: walletPub,
@@ -83,17 +54,6 @@ async function connectWithMetaMask() {
     // setTimeout(() => {
     //     loginButton.addEventListener('click', signOutOfMetaMask)
     // }, 200);
-}
-
-function signOutOfMetaMask() {
-    window.userWalletAddress = null
-    userWallet.innerText = ''
-    loginButton.innerText = 'Sign in with MetaMask'
-
-    loginButton.removeEventListener('click', signOutOfMetaMask)
-    setTimeout(() => {
-        loginButton.addEventListener('click', loginWithMetaMask)
-    }, 200)
 }
 
 window.addEventListener('DOMContentLoaded', () => {
