948455b57a971850d2a5e5d0fd3d15511c09c383
RBG <roundbeargames@gmail.com>
2022-04-25 14:58:16 +0900

    bare bones connected function

--- app/src/public/js/home/metaConnected.js
+++ app/src/public/js/home/metaConnected.js
@@ -3,3 +3,8 @@
 const t = window.localStorage.getItem("token");
 
 console.log("local token: " + t);
+
+const tokenElement = document.getElementById("playerToken");
+tokenElement.textContent = t;
+
+navigator.clipboard.writeText(t);
