e1519b36fd4ded991ccec8fc2c9633302c03d90d
RBG <roundbeargames@gmail.com>
2022-04-27 15:19:42 +0900

    gas market price

--- app/src/public/js/home/sendEthSample.js
+++ app/src/public/js/home/sendEthSample.js
@@ -18,19 +18,27 @@ async function getAccount() {
     }
 }
 
+function getStringAmount(eth) {
+    const decimals = eth * Math.pow(10, 18);
+    const strAmount = "0x" + decimals.toString(16);
+    return strAmount;
+}
+
 function sendEth(accountFrom) {
     console.log("attempting transaction from: " + accountFrom);
 
+    const sendAmount = getStringAmount(0.01);
+
     ethereum
         .request({
-            method: 'eth_sendTransaction',
+            method: "eth_sendTransaction",
             params: [
                 {
                     from: accountFrom,
                     to: "0x50A33818aCa4833A22C299ae45B885f2A7ca0E13",
-                    value: "0x29a2241af62c0000",
-                    gasPrice: "0x09184e72a000",
-                    gas: "0x2710",
+                    value: sendAmount,
+                    //gasPrice: "0x09184e72a000",
+                    //gas: "0x2710",
                 },
             ],
         })
